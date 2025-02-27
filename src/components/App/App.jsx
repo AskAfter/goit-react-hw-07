50297b270770db76bbf9d04a3fd84589997df9ff
AskAfter <askafter904@gmail.com>
2025-02-27 14:45:09 +0100

    render contacts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,6 +13,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchData());
+    console.log('first');
   }, [dispatch]);
   return (
     <div className={s.container}>
